90ad07d2cf0c87a245dba3d6cd049717e2a627e3
adarshhari2011 <141186004+adarshhari2011@users.noreply.github.com>
2023-09-26 17:53:59 -0400

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,22 @@
+def cal():
+  opp=int(input(" Enter 1 for addtion,\n Enter 2 for subtraction,\n Enter 3 for multpication, \n Enter 4 for division,\n "))
+  if opp==1:
+   NUM1=float(input("Put the first number."))
+   NUM2=float(input("Put the second number."))
+   print("The answer is", NUM1+NUM2)
+
+  elif opp==2:
+   NUM1=float(input("Put the first number."))
+   NUM2=float(input("Put the second number."))
+   print("The answer is", NUM1-NUM2)
+
+  elif opp==3:
+   NUM1=float(input("Put the first number."))
+   NUM2=float(input("Put the second number."))
+   print("The answer is", NUM1*NUM2)
+
+  elif opp==4:
+   NUM1=float(input("Put the first number."))
+   NUM2=float(input("Put the second number."))
+   print("The answer is", NUM1/NUM2)
+
